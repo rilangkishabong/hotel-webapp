74677184f8d4900a79b9d997c42b800f745aefff
rilangkishabong <rilangkishabong@gmail.com>
2022-05-08 16:20:14 +0530

    initial commit of hotelapp using core bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import Contact from "./pages/Contact";
+import Booknow from "./pages/Booknow";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/rooms/" element={<Rooms />} />
+          <Route path="/rooms/:slug" element={<SingleRoom />} />
+          <Route path="/booknow/:slug" element={<Booknow />} />
+          <Route component={Error} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
